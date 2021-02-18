7d7990d375435b38eddc0360efe166ba1fdb7892
BuzaAlx <buzakivi@gmail.com>
2021-02-18 19:04:04 +0200

    handling register/login resolvers

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -5,4 +5,7 @@ module.exports = {
   Query: {
     ...filmsResolvers.Query,
   },
+  Mutation: {
+    ...usersResolvers.Mutation,
+  },
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -10,7 +10,26 @@ module.exports = gql`
     sourceUrl: String!
   }
 
+  type User {
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+
   type Query {
     getFilms: [Film]
+  }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
   }
 `;
